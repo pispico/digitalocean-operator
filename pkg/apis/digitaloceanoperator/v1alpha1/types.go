02f4d6e8c20a5fb0bbef1c94a2ea6ba30b1795ce
William Pispico <williampispico@gmail.com>
2023-02-15 21:28:21 -0400

    first push

---
+++ pkg/apis/digitaloceanoperator/v1alpha1/types.go
@@ -0,0 +1,66 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Kluster is a specification for a Kluster resource
+type Kluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   KlusterSpec   `json:"spec,omitempty"`
+	Status KlusterStatus `json:"status,omitempty"`
+}
+
+// FooSpec is the spec for a Foo resource
+type KlusterSpec struct {
+	Name        string `json:"name,omitempty"`
+	Region      string `json:"region,omitempty"`
+	Version     string `json:"version,omitempty"`
+	TokenSecret string `json:"tokenSecret,omitempty"`
+
+	NodePools []NodePool `json:"nodePools,omitempty"`
+}
+
+type NodePool struct {
+	Size  string `json:"size,omitempty"`
+	Name  string `json:"name,omitempty"`
+	Count int    `json:"count,omitempty"`
+}
+
+// FooStatus is the status for a Foo resource
+type KlusterStatus struct {
+	KlusterID  string `json:"klusterID,omitempty"`
+	Progress   string `json:"progress,omitempty"`
+	KubeConfig string `json:"kubeConfig,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// FooList is a list of Foo resources
+type KlusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []Kluster `json:"items,omitempty"`
+}

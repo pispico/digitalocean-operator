596e6c8e75d528f5faf26971ee1985fe94427e0b
William Pispico <williampispico@gmail.com>
2023-02-15 22:35:50 -0400

    preparing to generate CRDs

--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/google/gnostic v0.5.7-v3refs // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
@@ -28,6 +29,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.3.1-0.20221206200815-1e63c2f08a10 // indirect
 	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b // indirect
 	golang.org/x/sys v0.3.0 // indirect
--- main.go
+++ main.go
@@ -17,13 +17,45 @@ limitations under the License.
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"log"
+	"path/filepath"
 
-	"github.com/pispico/digitalocean-operator/pkg/apis/pispico.dev/v1alpha1"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+
+	doclient "github.com/pispico/digitalocean-operator/pkg/client/clientset/versioned"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func main() {
-	teste := v1alpha1.Kluster{}
-	fmt.Println(teste)
+	var kubeconfig *string
+
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		log.Printf("Building config from flags, %s", err.Error())
+	}
+
+	doclientset, err := doclient.NewForConfig(config)
+	if err != nil {
+		log.Printf("getting doclientset %s\n", err.Error())
+	}
+
+	fmt.Println(doclientset)
+
+	klusters, err := doclientset.PispicoV1alpha1().Klusters("").List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		log.Printf("listing klusters%s\n", err.Error())
+	}
+	fmt.Printf("Number of klusters is %d\n", len(klusters.Items))
 
 }
